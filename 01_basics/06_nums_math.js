dab870f88d2a9f4e17ef53aa5209e0b2c9dd1e02
Sunit2003-mallick <sunitkumarmallick2003@gmail.com>
2026-05-12 08:56:30 +0530

    numbers & maths

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -1,3 +1,4 @@
+/*
 const score = 400
 console.log(score); // Output: 400
 
@@ -12,3 +13,31 @@ console.log(otherNumber.toPrecision(3)); // Output: 124
 
 const hundreds = 1000000
 console.log(hundreds.toLocaleString('en-IN')); // Output: 10,00,000
+*/
+
+
+
+// ++++++++++ Maths ++++++++++
+
+/*
+console.log(Math); // Output: Object [Math] {}
+console.log(Math.abs(-4)); // Output: 4
+console.log(Math.round(4.8)); // Output: 5
+console.log(Math.ceil(4.2)); // Output: 5
+// Rounding down to the nearest upperinteger
+console.log(Math.floor(4.2)); // Output: 4
+// Rounding down to the nearest lower integer
+console.log(Math.min(4, 3, 6, 8)); // Output: 3
+console.log(Math.max(4, 3, 6, 8)); // Output: 8
+*/
+
+console.log(Math.random());
+// Output: A random number between 0 (inclusive) and 1 (exclusive)
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
